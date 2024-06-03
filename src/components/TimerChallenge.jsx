4a721e3f36f8d3de5952d7a2cd0cc5f90f894eb1
Patrick Boutin <patrick.boutin@roberthalf.com>
2024-06-02 23:09:08 -0700

    In order for the modal's overlay to work we need to set open value on the modal programatically via showModal() built-in method. We remove the open attribute and condition since the modal will be hidden unless otherwise. In ResultModal, import forwardRef, wrap it around the main component function and store in variable which is then exported as default.. Also add second argument after object restructuring as ref then call showModal() within the handleStart() function.

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -2,7 +2,8 @@ import { useState, useRef } from 'react';
 import ResultModal from './ResultModal.jsx';
 
 function TimerChallenge({ title, targetTime }) {
-  const timer = useRef(null);
+  const timer = useRef();
+  const dialog = useRef();
 
   const [timerStarted, setTimerStarted] = useState(false);
   const [timerExpired, setTimerExpired] = useState(false);
@@ -10,13 +11,14 @@ function TimerChallenge({ title, targetTime }) {
   function handleStart() {
     timer.current = setTimeout(() => setTimerExpired(true), targetTime * 1000);
     setTimerStarted(true);
+    dialog.current.showModal();
   }
 
   const handleStop = () => clearTimeout(timer.current);
 
   return (
     <>
-      {timerExpired && <ResultModal targetTime={targetTime} result="lost" />}
+      <ResultModal ref={dialog} targetTime={targetTime} result="lost" />
       <section className="challenge">
         <h2>{title}</h2>
         <p className="challenge-time">{targetTime} second{targetTime > 1 ? 's' : ''}</p>
