b8afb5a0c4148939a57e10764573932ed00e306f
Patrick Boutin <patrick.boutin@roberthalf.com>
2024-06-03 11:24:36 -0700

    Added handleReset method and formatted remaining time with proper score to be shown conditionally if lost or not.

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -11,8 +11,11 @@ function TimerChallenge({ title, targetTime }) {
 
   if (timeRemaining <= 0) {
     clearInterval(timer.current);
-    setTimeRemaining(targetTime * 1000);
     dialog.current.open();
+  }
+
+  function handleReset() {
+    setTimeRemaining(targetTime * 1000);
   }
 
   function handleStart() {
@@ -28,7 +31,7 @@ function TimerChallenge({ title, targetTime }) {
 
   return (
     <>
-      <ResultModal ref={dialog} targetTime={targetTime} result="lost" />
+      <ResultModal ref={dialog} targetTime={targetTime} remainingTime={timeRemaining} onReset={handleReset} />
       <section className="challenge">
         <h2>{title}</h2>
         <p className="challenge-time">{targetTime} second{targetTime > 1 ? 's' : ''}</p>
