481bd6dea6f805ac8df0ba4e1b4a47e683576c94
Patrick Boutin <patrick.boutin@roberthalf.com>
2024-06-02 22:51:33 -0700

    Created ResultModal component and added it to the TimerChallenge component conditionally if timerExpired is true. Removed no longer needed paragraph.

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,4 +1,5 @@
 import { useState, useRef } from 'react';
+import ResultModal from './ResultModal.jsx';
 
 function TimerChallenge({ title, targetTime }) {
   const timer = useRef(null);
@@ -14,17 +15,19 @@ function TimerChallenge({ title, targetTime }) {
   const handleStop = () => clearTimeout(timer.current);
 
   return (
-    <section className="challenge">
-      <h2>{title}</h2>
-      {timerExpired && <p>You lost!!</p>}
-      <p className="challenge-time">{targetTime} second{targetTime > 1 ? 's' : ''}</p>
-      <p>
-        <button onClick={timerStarted ? handleStop : handleStart}>{timerStarted ? 'Stop' : 'Start'} Challenge</button>
-      </p>
-      <p className={timerStarted ? 'active' : undefined}>
-        {timerStarted ? 'Time is running...' : 'Timer inactive'}
-      </p>
-    </section>
+    <>
+      {timerExpired && <ResultModal targetTime={targetTime} result="lost" />}
+      <section className="challenge">
+        <h2>{title}</h2>
+        <p className="challenge-time">{targetTime} second{targetTime > 1 ? 's' : ''}</p>
+        <p>
+          <button onClick={timerStarted ? handleStop : handleStart}>{timerStarted ? 'Stop' : 'Start'} Challenge</button>
+        </p>
+        <p className={timerStarted ? 'active' : undefined}>
+          {timerStarted ? 'Time is running...' : 'Timer inactive'}
+        </p>
+      </section>
+    </>
   );
 }
 
